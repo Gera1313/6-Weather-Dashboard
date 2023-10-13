2439eb7a6644a3872f4e06df7b23ebf491f51adc
Mark Anthony <touya1313@hotmail.com>
2023-10-13 14:37:23 -0600

    selected html elements added to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 const form = document.getElementById('weatherForm');
+const cityInput = document.querySelector('.city-input');
+const currentWeather = document.querySelector('.current-weather .details');
+const fiveDayForecast = document.querySelector('.five-day-cards');
 
 var APIKey = "1f7ee33dc86217d48db8099bae79dad7";
 
